e2b7d99f4e286e690dbd36a6794e321c5cf20265
Udayan Sahai <udasahai@gmail.com>
2020-03-29 00:07:59 +0000

    changes

--- routes/track.js
+++ routes/track.js
@@ -47,7 +47,7 @@ router.post('/', function(req, res, next) {
     //         });
     // });
 
-    async function firstQuery() {
+    async function insertNewPing() {
 
         var datum = {
             site_id: req.body.site_id,
@@ -88,6 +88,75 @@ router.post('/', function(req, res, next) {
 
         let result = await db.query('INSERT INTO track SET ?', post);
         return result.insertId;
+    }
+
+
+    const getPreviousPing = async(insertId) => {
+
+        const query_string = "SELECT * FROM track where user_id=? and id!= ORDER BY DESC LIMIT 1?" //Get the previous ping, exlude the currently inserted one. 
+        const args = [req.body.user_id, insertId];
+
+        let result = await db.query(query_string, args);
+
+        let props = {};
+        props.result = result;
+        props.insertId = insertId
+
+        return props;
+    }
+
+
+    const fillCumalativeTablesParallel = async(props) => {
+
+        let result = props.result;
+        let insertId = props.insertId;
+
+        console.log(result)
+
+        if (result.length == 0)
+            return false;
+
+        let prev_timestamp = parseInt(result[0].timestamp); //get the timestamp of the previous ping
+        let time_bw_pings = parseInt(req.body.timestamp) - prev_timestamp //time between this ping and prev ping. 
+
+        if (time_bw_pings > timeout)
+            return false;
+
+        return await Promise.All(fillCumalativeTables(props));
+
+    }
+
+
+    const fillCumalativeTables = async(props) => {
+
+        let result = props.result;
+        let insertId = props.insertId;
+
+        let prev_timestamp = parseInt(result[0].timestamp); //get the timestamp of the previous ping
+        let time_bw_pings = parseInt(req.body.timestamp) - prev_timestamp //time between this ping and prev ping. 
+
+
+        //See if we can fill table 2 by comparing the site_id, user_id and tracking_type fields
+        if (req.body.user_id == result[0].user_id && req.body.site_id == result[0].site_id && req.body.tracking_type == result[0].tracking_type) {
+
+            let query_string = `SELECT total_time FROM user_type_total
+                                    WHERE site_id = ? and user_id = ? and tracking_type = ?)`
+            let args = [req.body.site_id, req.body.user_id, req.body.tracking_type]
+            let res = await db.query(query_string, args);
+
+            if (res.length == 0)
+                return false;
+
+            let total_time = res[0].total_time + time_bw_pings;
+
+            query_string = `UPDATE user_type_total SET ? WHERE site_id = ? and user_id = ? and tracking_type = ?`;
+            args = [{ last_track_id: insertId, total_time: total_time },
+                req.body.site_id, req.body.user_id, req.body.tracking_type
+            ]
+            return await db.query(query_string, args);
+        }
+
+        return false;
     }
 
     async function second(insertId) {
@@ -154,7 +223,9 @@ router.post('/', function(req, res, next) {
 
     }
 
-    firstQuery().then((insertId) => second(insertId)).catch(e => console.error(e));
+    // insertNewPing().then((insertId) => second(insertId)).catch(e => console.error(e));
+
+    let previous_ping = insertNewPing().then(insertId => getPreviousPing(insertId)).then(props => fillCumalativeTablesParallel(props)).catch(e => console.error(e));
 
     // var last_track_id_rows;
 
---
+++ run.sh
@@ -0,0 +1,1 @@
+nodemon /root/teamie/bin/www
